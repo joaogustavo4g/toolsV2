f594242962d5a2bcb988ea2acc9f3b87d0d7f17f
João Gustavo <joaosl33p@gmail.com>
2019-09-04 08:38:52 -0300

    fix bug

--- server.js
+++ server.js
@@ -12,5 +12,4 @@ app.use(cors())
 //LINKDB = Link para conexão do banco de dados, deve ser colocado no arquivo ".env"
 database.connect(process.env.LINKDB, { useNewUrlParser: true })
 
-
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3003)
