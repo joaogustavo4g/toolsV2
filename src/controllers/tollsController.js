9c61d2ef35a44cf412a5307c17b064abf6629b39
João Gustavo <joaosl33p@gmail.com>
2019-09-04 07:02:43 -0300

    comentario

---
+++ src/controllers/tollsController.js
@@ -0,0 +1,44 @@
+const toolsDataBase = require('../models/toolsModels');
+
+module.exports = {
+
+    async listar(req, res) {
+        const tag = req.query.tag;
+
+        //Verifica se exite tag na rota
+        if (tag) {
+            let tool = await toolsDataBase.find({ tags: tag })
+            return res.json({ tool })
+        }
+
+        let tool = await toolsDataBase.find()
+        return res.json({ tool })
+    },
+
+    async registrar(req, res) {
+        const { title } = req.body;
+        const toolExist = await toolsDataBase.findOne({ title });
+
+        // verifica se já existe uma tools com o mesmo nome
+        if (toolExist) {
+            return res.status(200).send({ Exist: toolExist })
+        }
+
+        const tool = await toolsDataBase.create(req.body)
+
+        return res.status(201).json({ tool })
+    },
+
+    async deletar(req, res) {
+        const id = req.params.id;
+
+        try {
+            await toolsDataBase.findByIdAndDelete({ _id: id })
+            return res.status(203).json({toolsDataBase});
+
+        } catch (error) {
+            return res.status(500).json({ error });
+        }
+
+    },
+}
