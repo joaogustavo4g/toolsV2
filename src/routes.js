2efc08328373b2fd06545456b4a48d60869ad8a1
João Gustavo <joaosl33p@gmail.com>
2019-09-12 11:26:22 -0300

    add new router

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,26 @@
 const express = require("express")
 const toolsController = require('./controllers/tollsController')
+const userController = require('./controllers/userController')
 
 const Router = express.Router()
 
+
+// rotas de users
+
+Router.post('/login', userController.login)
+Router.post('/cadastrar', userController.cadastrar)
+
+
+// rotas para as tools
 Router.get('/tools', toolsController.listar)
+
+// parte das rotas que necessita verificação
+
+//tolls
 Router.post('/tools', toolsController.registrar)
 Router.delete('/tools/:id', toolsController.deletar)
+
+//usuarios
 
     
 module.exports = Router;
