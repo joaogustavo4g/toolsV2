9ef8c87a0c44ba20b3a6de02d57e9c7e41bf2894
João Gustavo <joaosl33p@gmail.com>
2019-09-12 12:13:00 -0300

    conf Register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,35 @@
+const userDB = require('../models/userModels')
+
+
+const varredura = async (user, mail) => {
+    let userExist = await userDB.find({ user })
+    let mailExist = await userDB.find({ mail })
+
+    if (userExist || mailExist) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 module.exports = {
 
     async cadastrar(req, res) {
-        return res.status(201).json({user: "criado"})
+        let { user, mail } = req.body;
+
+        if (varredura(user, mail)) {
+            return res.status(203).json({ erro: "usuario já cadastrado" })
+        }
+
+        let newUser = await userDB.create(req.body)
+        newUser.password = "";
+
+        return res.status(201).json({ newUser })
     },
 
     async login(req, res) {
-        return res.status(201).json({user: "criado"})
+        console.log("criar login")
+        return res.status(201).json({ user: "criado" })
     }
 }
